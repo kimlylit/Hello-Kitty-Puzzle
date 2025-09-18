b544e2ea58a6bca19186245e46384e6f687324da
kimlylit <khiningyinmyat22@gmail.com>
2025-09-18 18:44:40 +0630

    Update puzzle.js

--- puzzle.js
+++ puzzle.js
@@ -8,7 +8,7 @@ var otherTile; //blank tile
 var turns = 0;
 
 // var imgOrder = ["1", "2", "3", "4", "5", "6", "7", "8", "white"];
-var imgOrder = ["4", "1", "3", "7", "2", "6", "white", "5", "8"];
+var imgOrder = ["1", "6", "2", "7", "4", "8", "white", "3", "5"];
 
 window.onload = function() {
     for (let r=0; r < rows; r++) {
@@ -87,3 +87,4 @@ function dragEnd(){
 
 
 }
+
